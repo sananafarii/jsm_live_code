0ea5a5c05483e6cffcaa77e3b902f101c55e4fc9
sana <sananafari1382@gmail.com>
2025-09-13 21:39:39 +0330

    fix:

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: [require('@tailwindcss/postcss'), require('autoprefixer')],
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
 };
-
-export default config;
