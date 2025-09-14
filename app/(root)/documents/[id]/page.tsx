df7b4d32fe4d9b19b577b022c874228d274145dd
sana <sananafari1382@gmail.com>
2025-09-14 19:49:28 +0330

    fix:

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,15 @@
 import { Editor } from '../../../components/editor/Editor';
+import Header from '../../../components/Header';
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center 
+        justify-center rounded-full bg-white/10 px-3 py-1">
+          <p className="document-title">This is a fake document title</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   )
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-type HeaderProps = {
-    children: React.ReactNode;
-}
 
 const Header = ( {children}: HeaderProps) => {
   return (
