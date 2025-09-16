98bd438956fcad9a4b74cf64b1e86c77afe7f738
sana <sananafari1382@gmail.com>
2025-09-16 16:01:58 +0330

    fix:

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from '../../../components/editor/Editor';
 import Header from '../../../components/Header';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 const Document = () => {
   return (
@@ -9,10 +10,19 @@ const Document = () => {
         justify-center rounded-full bg-white/10 px-3 py-1">
           <p className="document-title">This is a fake document title</p>
         </div>
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+       
       </Header>
       <Editor />
     </div>
   )
 }
+
 
 export default Document;
